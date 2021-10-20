15bc5c0a2651d1c4f92d5dcf719e578ad8fe4fbb
steven_jhu <stevenjhu5j@gmail.com>
2021-10-20 13:08:02 +0800

    MVC 官方

---
+++ MvcMovieLab/MvcMovie/Models/Data/MvcMovieContext.cs
@@ -0,0 +1,24 @@
+﻿//using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+using MySql.Data.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
+
+namespace MvcMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder
+        //    .Entity<Movie>().ToTable<Movie>("Movie");
+        //}
+    }
+}
+
---
+++ MvcMovieLab/MvcMovie/Models/MockStudentRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class MockStudentRepository : IStudentRepository
+    {
+        private List<Student> _studentList;
+        public MockStudentRepository()
+        {
+            throw new NotImplementedException();
+        }
+        public Student GetStudent(int id)
+        {
+            return _studentList.FirstOrDefault(a => a.Id == id);
+        }
+    }
+}
